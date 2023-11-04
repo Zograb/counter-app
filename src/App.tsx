d8612a817ecc2c151a1038de7fe0dc15646b9424
Zograb Mkrtchyan <mkrtch.zograb@gmail.com>
2023-11-04 22:54:05 +0400

    ✨ add app ui and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react';
+
+// Components
+import { Button } from '@/components';
 
 function App() {
-  const [count] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const decrement = useCallback(() => {
+    const newCount = count - 1;
+    setCount(newCount <= 0 ? 0 : newCount);
+  }, [count, setCount]);
+
+  const increment = useCallback(() => {
+    setCount(count + 1);
+  }, [count, setCount]);
+
+  const reset = useCallback(() => {
+    setCount(0);
+  }, [setCount]);
 
   return (
-    <div>{count}</div>
-  )
+    <div className="h-screen w-screen flex justify-center items-center flex-col">
+      <div className="flex justify-center items-center">
+        <Button onClick={decrement}>-</Button>
+        <div className="text-3xl min-w-[70px] px-4 text-center">{count}</div>
+        <Button onClick={increment}>+</Button>
+      </div>
+      <Button className="mt-8" onClick={reset}>RESET</Button>
+    </div>
+  );
 }
 
 export default App
